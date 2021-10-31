a8e042d7084dbc776280b79585d9a7ba13dd86ca
pacharamai <pacharamai@hospital-os.com>
2021-10-31 23:31:28 +0700

    [fix]: next.config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
-    domain: ['l4zy-p.github.io', 'localhost']
+    domains: ['l4zy-p.github.io', 'localhost']
   },
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
