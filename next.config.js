e4d212e7df0e9b8edac462c8437b3a9ca9d57a6d
pacharamai <pacharamai@hospital-os.com>
2021-10-31 23:14:58 +0700

    [fix]: next.config

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   images: {
     loader: 'imgix',
-    path: '',
-    domain: ['github.com']
+    path: process.env.NEXT_PUBLIC_BASE_PATH,
+    domain: ['l4zy-p.github.io']
   },
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
