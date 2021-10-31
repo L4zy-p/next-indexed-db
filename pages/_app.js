e52fdba3da505b805a5b2f87699ee19d520601f8
pacharamai <pacharamai@hospital-os.com>
2021-10-31 22:46:38 +0700

    [fix] package.json next.config and image url

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta httpEquiv='content-type' content='text/html; charset=utf-8' />
+      <link rel='icon' type='image/x-icon' href={`${process.env.NEXT_PUBLIC_BASE_PATH}/favicon.ico`} />
+      <title>Todo list</title>
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
